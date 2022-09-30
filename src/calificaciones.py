b86c7413a60c4cd2f48baf140e8de41cf17abb64
javcaragu1 <javcaragu1@alum.us.es>
2022-09-30 14:04:40 +0200

    Proyecto corregido con los condicionales de notas

---
+++ src/calificaciones.py
@@ -0,0 +1,63 @@
+def calcula_nota_cuestionario(aciertos, errores, total_respuestas):
+    nota = ((aciertos * 10) / total_respuestas) - ((errores * 10) / (50 - total_respuestas))
+    return nota
+
+def solicita_datos():
+    a = int(input("Introduzca el numero de aciertos: "))
+    e = int(input("Introduzca el numero de errores: "))
+    t_r = int(input("Introduzca el total de respuestas: "))
+
+    return a, e, t_r
+
+
+
+def calcula_nota_cuatrimestre(cuestionarios, examen, proyecto):
+    if proyecto < 5:
+        nota = 3
+    
+    else:
+        nota = 0.1 * sum(cuestionarios) + 0.6 * examen + 0.1 * proyecto
+    
+
+    return nota
+
+
+
+def calcula_nota_continua(cuestionarios, examenes, proyectos):
+    if calcula_nota_cuatrimestre((cuestionarios[0], cuestionarios[1], cuestionarios[2]), examenes[0], proyectos[0]) < 4 or calcula_nota_cuatrimestre((cuestionarios[3], cuestionarios[4], cuestionarios[5]), examenes[1], proyectos[1]) < 4:
+        nota = 4
+
+    else:
+        nota = (calcula_nota_cuatrimestre((cuestionarios[0], cuestionarios[1], cuestionarios[2]), examenes[0], proyectos[0]) + calcula_nota_cuatrimestre((cuestionarios[3], cuestionarios[4], cuestionarios[5]), examenes[1], proyectos[1])) / 2
+    
+
+    return nota
+
+
+
+
+def solicita_cuestionarios():
+    c1 = float(input("Introduzca la nota del cuestionario 1: "))
+    c2 = float(input("Introduzca la nota del cuestionario 2: "))
+    c3 = float(input("Introduzca la nota del cuestionario 3: "))
+    c4 = float(input("Introduzca la nota del cuestionario 4: "))
+    c5 = float(input("Introduzca la nota del cuestionario 5: "))
+    c6 = float(input("Introduzca la nota del cuestionario 6: "))
+
+    return (c1, c2, c3, c4, c5, c6)
+
+
+def solicita_examenes():
+    e1 = float(input("Introduzca la nota del primer examen: "))
+    e2 = float(input("Introduzca la nota del segundo examen: "))
+
+    return (e1, e2)
+
+
+def solicita_proyectos():
+    p1 = float(input("Introduzca la nota del primer proyecto: "))
+    p2 = float(input("Introduzca la nota del segundo proyecto: "))
+
+    return (p1, p2)
+
+
---
+++ src/test_calificaciones.py
@@ -0,0 +1,55 @@
+from calificaciones import *
+
+
+
+def test_solicita_datos():
+    print("Calculemos la nota de tu cuestionario.")
+    a, e, t_r = solicita_datos()
+    return a, e, t_r
+
+
+
+def test_calcula_nota_cuestionario(a, e, t_r):
+    print("Su nota en el cuestionario es: ", calcula_nota_cuestionario(a, e, t_r))
+
+
+
+
+def test_solicita_cuestionarios():
+    print("Notas de los cuestionarios.")
+    tupla = solicita_cuestionarios()
+    return tupla
+
+
+def test_solicita_examenes():
+    print("Notas de los examenes.")
+    tupla = solicita_examenes()
+    return tupla
+
+
+def test_solicita_proyectos():
+    print("Notas de los proyectos.")
+    tupla = solicita_proyectos()
+    return tupla
+
+
+def test_calcula_nota_cuatrimestre(cuestionarios, examen, proyecto):
+    print("Su nota del cuatrimestre es: ", calcula_nota_cuatrimestre(cuestionarios, examen, proyecto))
+
+
+def test_calcula_nota_continua(cuestionarios, examenes, proyectos):
+    print("Tu nota en la evaluacion continua es: ", calcula_nota_continua(cuestionarios, examenes, proyectos))
+
+if __name__ == "__main__":
+    
+
+    a, e, t_r = test_solicita_datos()
+    test_calcula_nota_cuestionario(a, e, t_r)
+
+
+    c = test_solicita_cuestionarios()
+    e = test_solicita_examenes()
+    p = test_solicita_proyectos()
+    test_calcula_nota_cuatrimestre((c[0], c[1], c[2]), e[0], p[0])
+    test_calcula_nota_cuatrimestre((c[3], c[4], c[5]), e[1], p[1])
+    test_calcula_nota_continua(c, e, p)
